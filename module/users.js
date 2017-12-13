57bb9b5f13b53ac7cb301ccc24bea6100ed7fc6f
matt <matt@shapeshift.io>
2017-12-12 23:57:33 -0700

    update for payments

--- module/users.js
+++ module/users.js
@@ -1,12 +1,27 @@
 
-const payments = require('./module/payments.js')
+let TAG = " | users | "
+
+const config = require('../secrets.js')
+const monk = require('monk')
+
+const db = require('monk')('localhost/dev')
+const users = db.get("users");
+const transactions = db.get("transactions");
+users.createIndex({id: 1}, {unique: true})
+transactions.createIndex({txid: 1}, {unique: true})
+
+
+const payments = require('./payments.js')
 
 
 module.exports = {
-    getBalance: function (user) {
-        return get_balance();
+    createUser: function (user) {
+        return create_user(user);
     },
-    // generate invoice
+    getBalance: function (user) {
+        return get_balance(user);
+    },
+    // credit user balance
     credit: function (user,amount,currency) {
         return credit_user(user,amount,currency);
     },
@@ -17,20 +32,43 @@ module.exports = {
 }
 
 
-const get_balance = async function(address) {
+const get_balance = async function(user) {
+    let tag = TAG+" | get_balance | "
+    let debug = true
     try {
+        if(!user) throw Error("101: missing user param!")
         let output = {}
+        if(debug) console.log(tag,"checkpoint1 ")
 
         //monk get user
-
+        let userInfo = await users.find({email:user})
+        if(debug) console.log(tag,"user: ",userInfo)
+        if(userInfo.length == 0) throw Error("102: unknown user!")
+        if(userInfo.length > 1) throw Error("103: multiple users with same email!")
+        userInfo = userInfo[0]
         //get address for user
-        let payments = await payments. paymentsByAddress('19eofCdUarH39CaK8nfkrzyaUHo8AcbjdT')
+        if(!userInfo.bitcoin) throw Error("104: invalid user!")
 
-        //if payments count > mongo tx count
+        //get payments
+        let bitcoinTxs = await payments.paymentsByAddress(userInfo.bitcoin)
+        if(!bitcoinTxs.txs) throw Error("105: invalid response from blockchain.info!")
+        if(debug) console.log(tag,"bitcoinTxs: ",bitcoinTxs)
+        bitcoinTxs = bitcoinTxs.txs
 
-        //lookup by txid
-            //goes txid exsit
-            //if not credit user and insert to mongo
+        for (let i = 0; i < bitcoinTxs.length; i++) {
+            if(!bitcoinTxs[i].hash) throw Error("106: invalid transaction!")
+            let txInfo = normalize_bitcoin_tx(bitcoinTxs[i],userInfo.bitcoin)
+            if(debug) console.log(tag,"txInfo: ",txInfo)
+            let result = await transactions.upsert(txInfo)
+            if(debug) console.log(tag,"txInfo: ",txInfo)
+            if(result.updated === 1 && result.status === "accepted"){
+                //credit user!
+                //amount satoshi to BTC
+                let amountBtc = txInfo.received * 100000000
+                credit_user(user,amountBtc,"BTC")
+            }
+
+        }
 
 
         return output
@@ -39,3 +77,50 @@ const get_balance = async function(address) {
         throw Error(err);
     }
 };
+
+//TODO
+
+//credit
+
+//debit
+
+let normalize_bitcoin_tx = function(tx,ourAddress){
+    let debug = true
+    let tag = " | normalize_bitcoin_tx | "
+    let output = {}
+    output.received = 0
+    output.confirmed = false
+    output.txid = tx.hash
+    output.status = "unknown"
+
+    //isConfirmed
+    if(tx.block_height){
+        output.confirmed == true
+    }
+
+
+    //scan outputs for payments
+    for (let i = 0; i < tx.out.length; i++) {
+        let outTx = tx.out[i]
+        if(debug) console.log(tag,"outTx: ",outTx)
+
+        // NOTE!: if rbf AND unconfirmed then DO NOT ACCEPT!
+        if (outTx.addr === ourAddress){
+            console.log("our address! payment! yay!")
+            if(debug) console.log(tag,"tx.rbf: ",tx.rbf)
+            if(debug) console.log(tag,"outTx.confirmed: ",outTx.confirmed)
+
+            if(tx.rbf && !outTx.confirmed){
+                output.status = "RBF! untrusted!"
+            } else {
+                output.received = output.received + output.value
+                output.status = "accepted"
+            }
+
+        } else{
+            console.log("not our address! probably change!")
+        }
+    }
+
+    return output
+}
--- test-module.js
+++ test-module.js
@@ -1,15 +1,28 @@
+
 const blockchain = require('blockchain.info')
 
 
 const payments = require('./module/payments.js')
-
-/*
-    Notes: each customer is an account, each invoice has its own address
- */
-
-const customer = 1
-
-//1Mp919RSyakLm9juyzwDicVXBVdxvqw9Mb
+const users = require('./module/users.js')
+//
+// /*
+//     Notes: each customer is an account, each invoice has its own address
+//  */
+//
+//
+//
+//
+//
+//
+// //const customer = 1
+//
+// //1Mp919RSyakLm9juyzwDicVXBVdxvqw9Mb
+//
+let user = "bithighlander@gmail.com"
+users.getBalance(user)
+    .then(function(resp){
+        console.log("resp: ",resp)
+    })
 
 
 //get address
@@ -24,14 +37,22 @@ const customer = 1
 //         console.log("resp: ",resp)
 //     })
 
-// lookup payment for customer
-payments. paymentsByAddress('19eofCdUarH39CaK8nfkrzyaUHo8AcbjdT')
-    .then(function(resp){
-        console.log("resp: ",resp)
-        console.log("resp: ",resp.txs[0].inputs)
-        console.log("resp: ",resp.txs[0].out)
-    })
 
+// lookup payment for customer
+// payments. paymentsByAddress('19eofCdUarH39CaK8nfkrzyaUHo8AcbjdT')
+//     .then(function(resp){
+//         console.log("resp: ",resp)
+//         console.log("resp: ",resp.txs[0].inputs)
+//         console.log("resp: ",resp.txs[0].out)
+//     })
+
+
+// payments. paymentsByAddress('15k8hyqdL5XbXs1e1hyDGXbBygvwWdnbun')
+//     .then(function(resp){
+//         console.log("resp: ",resp)
+//         console.log("resp: ",resp.txs[0].inputs)
+//         console.log("resp: ",resp.txs[0].out)
+//     })
 
 // lookup global payments
 
